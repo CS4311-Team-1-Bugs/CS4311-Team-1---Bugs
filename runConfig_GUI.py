3ee1bbe5697b15222bd56aa3a63c85987965f95b
carlosmontgomery <43247962+carlosmontgomery@users.noreply.github.com>
2021-02-23 23:34:23 -0700

    Update runConfig_GUI.py
    Added text fields: Run Name, Run Description, Whitelist IP Target, Blacklist IP Target.
    Added "Save" & "Cancel" buttons.
    Added default placeholders to all text fields.
    Version 0.0.2

--- runConfig_GUI.py
+++ runConfig_GUI.py
@@ -17,58 +17,78 @@ def runConfigWindow():
     textFieldRunName = QLineEdit(runConfigWin)
     textFieldRunName.move(95, 20)
     textFieldRunName.adjustSize()
+    textFieldRunName.setPlaceholderText("Run Name Default")
+
     # [SRS 7c]
     textRunDes = QLabel(runConfigWin)
     textRunDes.setText("Run Description: ")
-    textRunDes.move(15, 35)
+    textRunDes.move(15, 45)
+    textFieldRunDes = QLineEdit(runConfigWin)
+    textFieldRunDes.move(120, 50)
+    textFieldRunDes.adjustSize()
+    textFieldRunDes.setPlaceholderText("Run Description Default")
     # [SRS 7d]
     textWLIP = QLabel(runConfigWin)
     textWLIP.setText("Whitelisted IP Target: ")
-    textWLIP.move(15, 60)
+    textWLIP.move(15, 90)
     textWLIP.adjustSize()
+    textFieldWLIP = QLineEdit(runConfigWin)
+    textFieldWLIP.move(150, 85)
+    textFieldWLIP.adjustSize()
+    textFieldWLIP.setPlaceholderText("Whitelist IP Default")
     # [SRS 7e]
     textBLIP = QLabel(runConfigWin)
     textBLIP.setText("Blacklisted IP Target: ")
-    textBLIP.move(15, 80)
+    textBLIP.move(15, 120)
     textBLIP.adjustSize()
+    textFieldBLIP = QLineEdit(runConfigWin)
+    textFieldBLIP.move(150, 120)
+    textFieldBLIP.setPlaceholderText("Blacklist IP Default")
+    textFieldBLIP.adjustSize()
 
     # [SRS 7f]
     textScanType = QLabel(runConfigWin)
     textScanType.setText("Scan Type:")
-    textScanType.move(15, 100)
+    textScanType.move(15, 160)
     ScanType = QComboBox(runConfigWin)
     # [SRS 7g]
     ScanType.addItem("Scan Type")
     ScanType.addItem("Scan Type 1")
     ScanType.addItem("Scan Type 2")
-    ScanType.move(95,100)
+    ScanType.move(95,160)
     ScanType.adjustSize()
 
     # [SRS 7h]
     textOr = QLabel(runConfigWin)
     textOr.setText("OR")
-    textOr.move(75, 130)
+    textOr.move(75, 190)
 
     # [SRS 7i]
     textConfigFile = QLineEdit(runConfigWin)
-    textConfigFile.move(165, 160)
+    textConfigFile.move(165, 220)
     textConfigFile.adjustSize()
     textConfigFile.setPlaceholderText("Configuration File Default")
 
     textFileTitle = QLabel(runConfigWin)
     textFileTitle.setText("Run Configuration File")
-    textFileTitle.move(15,160)
+    textFileTitle.move(15,225)
     textFileTitle.adjustSize()
     # RunConfigFile Button
     buttonConfigFile = QPushButton(runConfigWin)
     buttonConfigFile.setText("Browse...")
-    buttonConfigFile.move(295, 156)
+    buttonConfigFile.move(295, 215)
     buttonConfigFile.adjustSize()
 
     #[SRS 7j]
     buttonSave = QPushButton(runConfigWin)
     buttonSave.setText("Save")
-    buttonSave.move(200, 300)
+    buttonSave.move(280, 260)
+
+
+    buttonCancel = QPushButton(runConfigWin)
+    buttonCancel.setText("Cancel")
+    buttonCancel.move(380, 260)
+
 
     ScanType.show()
     runConfigWin.show()
