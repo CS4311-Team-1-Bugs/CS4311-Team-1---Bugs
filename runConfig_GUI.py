51fd056cc548cf330858326fd05cc6a48e0d9210
carlosmontgomery <43247962+carlosmontgomery@users.noreply.github.com>
2021-03-10 19:39:50 -0700

    Update runConfig_GUI.py

--- runConfig_GUI.py
+++ runConfig_GUI.py
@@ -1,8 +1,10 @@
 import sys
+import datetime
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QComboBox, QLineEdit, QPushButton, QHBoxLayout, \
     QVBoxLayout, QFormLayout
-class RunConfigWindow(QWidget):
+class RunConfigWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         # init the window
         super(RunConfigWindow, self).__init__(*args, **kwargs)
@@ -10,9 +12,19 @@ class RunConfigWindow(QWidget):
         # Set the Window Title
         self.setWindowTitle("Configuration of the Selected Run")
 
-        # Create Outer base layer
-        outerLayout = QVBoxLayout()
+        menuWidget = QWidget()
+        menuLayout = self.makeMenuLayout()
+        menuWidget.setLayout(menuLayout)
+        self.setMenuWidget(menuWidget)
 
+        mainWidget = QWidget()
+        mainLayout = self.makeMainLayout()
+
+        mainWidget.setLayout(mainLayout)
+        self.setCentralWidget(mainWidget)
+
+
+    def makeMenuLayout(self):
         menuLayout = QVBoxLayout()
 
         # Title component of menu
@@ -35,45 +47,83 @@ class RunConfigWindow(QWidget):
         hButtons = QWidget()
         hButtons.setLayout(hLayout)
         menuLayout.addWidget(hButtons)
+        return menuLayout
 
+
+    def makeMainLayout(self):
+        outerLayout = QVBoxLayout()
         # Create Run Config Details layer
         runConfigLayout = QFormLayout()
-        runName = QLineEdit()
+        orLabel = self.orLabel()
+        orLabel1 =self.orLabel()
+        orLabel2 = self.orLabel()
+        dateTimeObj = datetime.datetime.now()
+        timestampStr = dateTimeObj.strftime("%I:%M %m/%d/%Y %p")
+        runName = QLabel()
         runName.setAlignment(Qt.AlignLeft)
-        runName.setPlaceholderText("Run Name Default")
+        runName.setText(timestampStr)
+        serifFont = QFont("Arial", 15)
+        runName.setFont(serifFont)
         runConfigLayout.addRow("Run Name:", runName)
         runDesc= QLineEdit()
         runDesc.setPlaceholderText("Run Description Default")
         runConfigLayout.addRow("Run Description:", runDesc)
+        buttonConfigFile = QPushButton("Browse")
+        buttonConfigFile.setStyleSheet("background-color:rgb(144,184,239)")
         WLIPtext = QLineEdit()
+        WLIPtextfile = QLineEdit()
         WLIPtext.setPlaceholderText("Whitelist IP Default")
         runConfigLayout.addRow("Whitelisted IP Target:", WLIPtext)
+        runConfigLayout.addWidget(orLabel)
+        runConfigLayout.addRow("Browse for Whitelist File:", WLIPtextfile)
+        runConfigLayout.addWidget(QPushButton("Browse"))
         BLIPtext = QLineEdit()
+        BLIPtextfile = QLineEdit()
         BLIPtext.setPlaceholderText("Blacklist IP Default")
         runConfigLayout.addRow("Blacklisted IP Target:", BLIPtext)
+        runConfigLayout.addWidget(orLabel1)
+        runConfigLayout.addRow("Browse for Blacklist File:", BLIPtextfile )
+        runConfigLayout.addWidget(QPushButton("Browse"))
         ScanType = QComboBox()
         scanList = ["Scan Type", "Scan Type 1", "Scan Type 2 ", "Scan Type 3"]
         ScanType.addItems(scanList)
         runConfigLayout.addRow("Scan Type:", ScanType)
-        runConfigLayout.addRow("OR", QLabel())
-        buttonConfigFile = QPushButton("Browse ...")
+        runConfigLayout.addWidget(orLabel2)
         ConfigFile = QLineEdit()
         ConfigFile.setPlaceholderText("Run Configuration File")
-        runConfigLayout.addRow("Run Configuration File", ConfigFile)
+        runConfigLayout.addRow("Browse for Run Configuration File:", ConfigFile,)
         runConfigLayout.addWidget(buttonConfigFile)
 
+
+
+        buttonWidget = QWidget()
+        buttonLayout = self.makeButtonLayout()
+        buttonWidget.setLayout(buttonLayout)
+
+        runConfigLayout.addWidget(buttonWidget)
+        runConfigLayout.setVerticalSpacing(20)
+        runConfigLayout.setHorizontalSpacing(10)
+
+
+        return runConfigLayout
+
+    def makeButtonLayout(self):
         # set button layout
         buttonLayout = QHBoxLayout()
         buttonLayout.addStretch(1)
         buttonLayout.addWidget(QPushButton("Save"))
         buttonLayout.addWidget(QPushButton("Cancel"))
+        buttonLayout.addSpacing(5)
+        buttonLayout.setAlignment(Qt.AlignBottom)
+        return buttonLayout
 
-
-        outerLayout.addLayout(menuLayout)
-        outerLayout.addLayout(runConfigLayout)
-        outerLayout.addLayout(buttonLayout)
-        self.setLayout(outerLayout)
-
+    def orLabel(self):
+        orLabel = QLabel()
+        orLabel.setText("OR")
+        orLabel.setAlignment(Qt.AlignCenter)
+        serifFont = QFont("Arial", 18)
+        orLabel.setFont(serifFont)
+        return orLabel
 
 
 if __name__ == "__main__":
