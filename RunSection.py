4484460e68928675bd0241ed949bb3f0b7cd7d9e
carlosmontgomery <43247962+carlosmontgomery@users.noreply.github.com>
2021-04-07 22:06:02 -0600

    Updated RunSection.py

--- RunSection.py
+++ RunSection.py
@@ -15,6 +15,13 @@ class RunSection():
         self.run_config()
         
     def run_config(self):
+
+        # Establish Connection to MongoDB - Run Config
+        client = pymongo.MongoClient(
+            "mongodb+srv://aaron:EDVsK1hnYHJEWZry@seacluster.f3vdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        db = client[ 'Test' ]
+        self.config = db[ "Run Config" ]
+
          # Title component of menu
         menuTitle = QLabel()
         menuTitle.setText("  Run Configuration  ")
@@ -23,86 +30,124 @@ class RunSection():
         menuTitle.setAlignment(Qt.AlignLeft)
     
         outerLayout = QVBoxLayout()
+
         # Create Run Config Details layer
         runConfigLayout = QFormLayout()
         orLabel = util.orLabel()
-        orLabel1 =util.orLabel()
+        orLabel1 = util.orLabel()
         orLabel2 = util.orLabel()
-        
-        #Run Name
+
+        # Run Name
         dateTimeObj = datetime.datetime.now()
         timestampStr = dateTimeObj.strftime("%I:%M %m/%d/%Y %p")
-        runName = QLabel()
-        runName.setAlignment(Qt.AlignLeft)
-        runName.setText(timestampStr)
+        self.runName = QLabel()
+        self.runName.setAlignment(Qt.AlignLeft)
+        self.runName.setText(timestampStr)
         serifFont = QFont("TimesNewRoman", 15)
-        runName.setFont(serifFont)
-        runConfigLayout.addRow("Run Name:", runName)
-        
-        #Run Description
-        runDesc= QLineEdit()
-        runDesc.setPlaceholderText("Run Description Default")
-        runConfigLayout.addRow("Run Description:", runDesc)
-        
-        WLIPtext = QPlainTextEdit()
-        WLIPtext.setPlaceholderText("Whitelist IP Default")
-        
+        self.runName.setFont(serifFont)
+        runConfigLayout.addRow("Run Name:", self.runName)
+
+        # Run Description
+        self.runDesc = QPlainTextEdit()
+        self.runDesc.setPlaceholderText("Run Description Default")
+        runConfigLayout.addRow("Run Description:", self.runDesc)
+
+        # Whitelist
+        self.WLIPtext = QPlainTextEdit()
+        self.WLIPtext.setPlaceholderText("Whitelist IP Default")
+
         BrowseChoiceLayout = QHBoxLayout()
-        BrowseChoiceLayout.addWidget(WLIPtext)
+        BrowseChoiceLayout.addWidget(self.WLIPtext)
         BrowseChoiceLayout.addWidget(orLabel)
         BrowseChoiceLayout.addWidget(QLabel("Browse for Whitelist Files"))
-        BrowseChoiceLayout.addWidget(QLineEdit())
-        BrowseChoiceLayout.addWidget(QPushButton("Browse"))
-        #buttonConfigFile = QPushButton("Browse")
+        self.path = QLineEdit()
+        BrowseChoiceLayout.addWidget(self.path)
+        wlipBrowse = QPushButton("Browse")
+        BrowseChoiceLayout.addWidget(wlipBrowse)
+        # buttonConfigFile = QPushButton("Browse")
+        wlipBrowse.clicked.connect(lambda: self.buttons("Browse",self.path))
         BrowseWidget = QWidget()
         BrowseWidget.setLayout(BrowseChoiceLayout)
-      
-        
-        BLIPtext = QPlainTextEdit()
-        BLIPtext.setPlaceholderText("Blacklist IP Default")
+
+        self.BLIPtext = QPlainTextEdit()
+        self.BLIPtext.setPlaceholderText("Blacklist IP Default")
+
         BrowseChoiceLayout2 = QHBoxLayout()
-        BrowseChoiceLayout2.addWidget(BLIPtext)
+        BrowseChoiceLayout2.addWidget(self.BLIPtext)
         BrowseChoiceLayout2.addWidget(orLabel1)
         BrowseChoiceLayout2.addWidget(QLabel("Browse for Blacklist Files"))
-        BrowseChoiceLayout2.addWidget(QLineEdit())
-        BrowseChoiceLayout2.addWidget(QPushButton("Browse"))
-        #buttonConfigFile = QPushButton("Browse")
+        self.bPath = QLineEdit()
+        BrowseChoiceLayout2.addWidget(self.bPath)
+        blipBrowse = QPushButton("Browse")
+        BrowseChoiceLayout2.addWidget(blipBrowse)
+        blipBrowse.clicked.connect(lambda: self.buttons("Browse",self.bPath))
         BrowseWidget2 = QWidget()
         BrowseWidget2.setLayout(BrowseChoiceLayout2)
- 
-        
+
         runConfigLayout.addRow("Whitelisted IP Target:", BrowseWidget)
         runConfigLayout.addRow("Blacklisted IP Target:", BrowseWidget2)
-        
-        
+
         ScanType = QComboBox()
-        scanList = ["Scan Type", "Scan Type 1", "Scan Type 2 ", "Scan Type 3"]
+        scanList = [ "Scan Type", "Scan Type 1", "Scan Type 2 ", "Scan Type 3" ]
         ScanType.addItems(scanList)
         runConfigLayout.addRow("Scan Type:", ScanType)
         runConfigLayout.addWidget(orLabel2)
-        ConfigFile = QLineEdit()
-        ConfigFile.setPlaceholderText("Run Configuration File")
-        runConfigLayout.addRow("Browse for Run Configuration File:", ConfigFile,)
-        runConfigLayout.addWidget(QPushButton("Browse"))
+        self.ConfigFile = QLineEdit()
+        self.ConfigFile.setPlaceholderText("Run Configuration File")
+        runConfigLayout.addRow("Browse for Run Configuration File:", self.ConfigFile, )
+        configButton = QPushButton("Browse")
+        runConfigLayout.addWidget(configButton)
+        configButton.clicked.connect(lambda: self.buttons("Browse",self.ConfigFile))
 
-
-
-        
         buttonWidget = util.make_saveCancel(self)
 
         runConfigLayout.addWidget(buttonWidget)
         runConfigLayout.setVerticalSpacing(20)
         runConfigLayout.setHorizontalSpacing(10)
+
         main = QWidget()
         main.setLayout(runConfigLayout)
-        
+
         self.runConfiguration = QDockWidget()
         self.runConfiguration.setTitleBarWidget(menuTitle)
         self.runConfiguration.setWidget(main)
         self.win.addDockWidget(Qt.RightDockWidgetArea, self.runConfiguration)
         self.runConfiguration.setVisible(False)
-        return 
+        return
+
+
+    def buttons(self, buttonName,button):
+        if "Browse" in buttonName:
+            if buttonName == "Browse":
+                fname = QFileDialog.getExistingDirectory(None, "Select a Directory...")
+            else:
+                fname = QFileDialog.getOpenFileName(None, "Select a file...", "./", filter="*(All Files)")
+            if isinstance(fname, tuple):
+                button.setText(str(fname[ 0 ]))
+            else:
+                button.setText(str(fname))
+        elif "Save" in buttonName:
+            name = self.runName.text()
+            description = self.runDesc.toPlainText()
+            wlipText = self.WLIPtext.toPlainText()
+            wlipFile =self.path.text()
+            blipText = self.BLIPtext.toPlainText()
+            blipFile = self.bPath.text()
+            runFile = self.ConfigFile.text()
+            inputStr = {"Run Name":name,"Run Description":description,"Target Whitelist":wlipText,"Whitelist File": wlipFile,"Target Blacklist":blipText,"Blacklist File":blipFile,"Run Configuration File":runFile}
+            self.config.insert_one(inputStr)
+        elif "Cancel" in buttonName:
+            self.runName.setText("")
+            self.runDesc.setPlainText("")
+            self.WLIPtext.setPlainText("")
+            self.BLIPtext.setPlainText("")
+            self.bPath.setText("")
+            self.path.setText("")
+            self.ConfigFile.setText("")
+
     def hide(self):
         self.runConfiguration.setVisible(False)
     def show(self):
         self.runConfiguration.setVisible(True)
+
+
